b92a197fcaaf59a30a2ae078ce776d8941d6e1df
Hao <hao.liu@daisee.com>
2024-06-05 12:08:49 +1000

    Add CI

--- flake.nix
+++ flake.nix
@@ -2,5 +2,18 @@
   description = "A `flake-parts` module for including `just` in devShell";
   outputs = _: {
     flakeModule = import ./flake-module.nix;
+
+    # https://github.com/srid/nixci
+    nixci.default = let overrideInputs = { just-flake = ./.; }; in {
+      dev = {
+        inherit overrideInputs;
+        dir = "dev";
+      };
+
+      test = {
+        inherit overrideInputs;
+        dir = "test";
+      };
+    };
   };
 }
